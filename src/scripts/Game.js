16fc8dcf2606ed6e8e7a9072f26abece873657cb
Mostafa Magdy <mostafa_magdy@fci.helwan.edu.eg>
2019-10-13 16:33:54 +0200

    add function CreatePlayer to Game

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -1,16 +1,32 @@
-import { easyLevel } from './Levels';
+// import {
+//   easyLevel
+// } from './Levels';
 
 class Game {
   static player = {};
 
   static score = 0;
 
-  static level = easyLevel;
+  // static level = easyLevel;
 
   /**
    * Creates a new instance of Enemy and set it's starting position
    */
   static createEnemy = () => {};
+
+  /**
+   *  Creates a new instance of Enemy and set it's starting position
+   */
+
+  static createPlayer = () => {
+    let pplayer = new Player("mostafa");
+    pplayer.playerElement = document.getElementById('player');
+    pplayer.playerElement.style.top = (window.innerHeight / 2 - pplayer.playerElement.height) + 'px';
+    document.addEventListener('keydown', event => {
+      console.log(event.keyCode);
+      pplayer.move(event.keyCode);
+    });
+  };
 
   /**
    * Inits player object, setInterval for creating enemies depending on the current level
@@ -27,6 +43,8 @@ class Game {
    * Ends the game once the player health is 0
    */
   static checkGameStatus = () => {};
+
+
 }
 
-export default Game;
+// export default Game;
