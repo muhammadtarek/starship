ec5a583dfca60c0c2324280087a098ae42879dd0
Mostafa Magdy <mostafa_magdy@fci.helwan.edu.eg>
2019-10-13 16:33:22 +0200

    add move function for player

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,5 +1,5 @@
 // @ts-check
-import GameObject from './GameObject';
+// import GameObject from './GameObject';
 
 class Player extends GameObject {
   /**
@@ -7,9 +7,30 @@ class Player extends GameObject {
    */
   constructor(name) {
     super(100);
-
+    this.playerSpeed = 30;
+    this.playerElement = null;
     this.name = name;
   }
+  //move function for Player
+  move = (eventCode) => {
+    switch (eventCode) {
+      case 38:
+        if (parseInt(this.playerElement.style.top.slice(0, -2)) >= 10) {
+          this.playerElement.style.top = parseInt(this.playerElement.style.top.slice(0, -2)) - this.playerSpeed + 'px';
+        }
+        break;
+      case 40:
+        if (parseInt(this.playerElement.style.top.slice(0, -2)) + this.playerElement.height <= window.innerHeight - 30) {
+          this.playerElement.style.top = parseInt(this.playerElement.style.top.slice(0, -2)) + this.playerSpeed + 'px';
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+
+
 }
 
-export default Player;
+// export default Player;
